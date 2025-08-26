e44e8a42d3c4c1d26ee8a2dc051e838a837dcd52
Spectella <spectella@gmail.com>
2025-08-27 02:13:43 +0700

    Updated device checking

--- vosk_model/checkdevice.py
+++ vosk_model/checkdevice.py
@@ -1,21 +1,25 @@
 import pyaudio
 
-# Create an instance of PyAudio
-p = pyaudio.PyAudio()
+def handle_devices(type: str, num_devices: int, p):
+    for i in range(num_devices):
+        device_info = p.get_device_info_by_index(i)
+        if device_info.get(type) > 0:
+            print(f"  Index: {device_info['index']}, Name: {device_info['name']}")
+        
+def main():
+    indent = ""
+    p = pyaudio.PyAudio()
+    num_devices = p.get_device_count()
+    
+    print("Available Input Devices:")
+    handle_devices('maxInputChannels', num_devices, p)
 
-# Get the total number of audio devices
-num_devices = p.get_device_count()
+    for _ in range(4): print(indent)
 
-print("Available Output Devices:")
+    print('Available Output Devices:')
+    handle_devices('maxOutputChannels', num_devices, p)
 
-# Iterate through all devices
-for i in range(num_devices):
-    # Get device information by index
-    device_info = p.get_device_info_by_index(i)
-
-    # Check if the device has output channels
-    if device_info.get('maxInputChannels') > 0:
-        print(f"  Index: {device_info['index']}, Name: {device_info['name']}")
-
-# Terminate the PyAudio instance
-p.terminate()
+    p.terminate()
+    
+if __name__ == "__main__":
+    main()
